986a2448276d308ba21341654234e931980b9641
Jprimero15 <jprimero15@aospa.co>
2023-05-24 02:07:00 +0000

    aospa: Revive backuptool support for vanilla builds
    * imported from current lineage backuptools and modified for AOSPA.
    
    Change-Id: Ib6995bda39818862d2947159cd81ac960b9652a7
    [Jprimero15: Update to vauxite and add only for vanilla builds]
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -40,6 +40,18 @@ PRODUCT_PACKAGES += \
     Glimpse
 
 ifeq ($(TARGET_DISABLES_GMS), true)
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/aospa/prebuilts/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/aospa/prebuilts/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/aospa/prebuilts/bin/50-aospa.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-aospa.sh
+ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
+PRODUCT_COPY_FILES += \
+    vendor/aospa/prebuilts/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
+    vendor/aospa/prebuilts/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
+    vendor/aospa/prebuilts/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
+endif
+
 # Vanilla apps
 PRODUCT_PACKAGES += \
     AvatarPicker \
