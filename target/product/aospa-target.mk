404bb2897acd3bd5ecefb4d975ca464efe872369
Jprimero15 <jprimero15@aospa.co>
2023-05-26 01:40:24 +0000

    aospa: Add more ringtones for vanilla build
    * based on lineage and aosp audio
    * also set default ringtones
    
    Change-Id: I96281889d061b145032e1d3253c43a44c2a61823
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -90,7 +90,17 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.config.media_vol_steps=30
 
 ifeq ($(TARGET_DISABLES_GMS), true)
-$(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
+# Include AOSP audio files
+include vendor/aospa/prebuilts/media/audio/aosp_audio.mk
+
+# Include PA audio files
+include vendor/aospa/prebuilts/media/audio/pa_audio.mk
+
+# Default notification/alarm sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone=Orion.ogg \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Hassium.ogg
 endif
 
 PRODUCT_COPY_FILES += \
