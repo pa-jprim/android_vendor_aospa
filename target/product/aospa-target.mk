2b2113887b0b07f9fb46534ffc151ce54e8a6ccd
Richard Raya <rdxzv.dev@gmail.com>
2024-06-25 19:40:49 -0300

    animations: pixel-charger: Move to product
    Change-Id: I41d5dea02a90d2b5de1abaaa2eff43bca9edf1de
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -97,7 +97,7 @@ $(call inherit-product, vendor/aospa/bootanimation/bootanimation.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
-    charger_res_images_vendor_pixel
+    charger_res_images_product_pixel
 
 # curl
 PRODUCT_PACKAGES += \
